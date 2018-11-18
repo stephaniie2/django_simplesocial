f72f3f78518d18c9c3803534aaa6d7afa19ce428
stephyswe <stephaniiehallberg@gmail.com>
2018-11-18 14:37:26 +0100

    adding login and logout views

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('signup/',views.SignUp.as_view(),name='signup')
 
+
 ]
--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIFILES_DIR = [os.path.join(BASE_DIR,'static')]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
